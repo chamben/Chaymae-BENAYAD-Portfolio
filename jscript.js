c3c73adbe1acd62c4fd9f953228d9f9f237160d5
BENAYAD Chaymae <chaymae.benayad@etu.uae.ac.ma>
2022-11-11 00:22:29 +0100

    fetched my projects data from my fake api

--- jscript.js
+++ jscript.js
@@ -5,10 +5,12 @@ const navLink = navbar.querySelectorAll("a");
 const allSections = document.querySelectorAll(".section");
 const navbarBtn = document.querySelector(".navbar-btn");
 const sidebar = document.querySelector(".sideBar");
+const projectsContainer = document.querySelector("#project-container");
 
 //Preloader
 window.addEventListener("load", () => {
   preloader.classList.add("hidden");
+  getAllprojectsData();
 });
 
 //Navigate through the navbar anchor links
@@ -45,5 +47,53 @@ const showSidebar = function () {
   sidebar.classList.toggle("show");
   navbarBtn.classList.toggle("show");
 };
-
 navbarBtn.addEventListener("click", showSidebar);
+
+//get my projects data from my fake api
+const getAllprojectsData = async function () {
+  const response = await fetch(
+    "https://chamben.github.io/projects-fake-api/projectsData.json"
+  );
+  const data = await response.json();
+  console.log(data);
+  const projectsData = data.map((project, index) => {
+    return {
+      id: index + 1,
+      imgUrl: `images/projects/project-cover-${index + 1}.png`,
+      title: project.title,
+      description: project.description,
+      demoUrl: project.demoUrl,
+      githubUrl: project.githubUrl,
+    };
+  });
+  displayProjects(projectsData);
+};
+
+// display projects data
+const displayProjects = function (projects) {
+  projects.map((project) => {
+    const html = `
+    <div class="project-item padding-15">
+       <div class="project-item-content dark-shadow">
+          <div class="project-img">
+            <img src=${project.imgUrl} alt="project" />
+          </div>
+          <div class="project-info">
+            <div class="demo-btns">
+              <a target="_blank" href=${project.demoUrl}>
+                <i class="fa fa-eye"></i>
+              </a>
+              <a target="_blank" href=${project.githubUrl}>
+                <i class="fa fa-github"></i>
+              </a>
+            </div>
+          </div>
+          <div class="project-description">
+            <span class="project-title">${project.title}&nbsp</span>${project.description}
+          </div>
+       </div>
+      </div>
+    `;
+    projectsContainer.insertAdjacentHTML("beforeend", html);
+  });
+};
